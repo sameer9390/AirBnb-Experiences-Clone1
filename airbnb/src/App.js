084f04b306d274500047e6e8bd03ae9ea812b30f
sameer9390 <sameerofficial058@gmail.com>
2023-01-06 17:01:50 +0530

    first commit

---
+++ airbnb/src/App.js
@@ -0,0 +1,34 @@
+import logo from './logo.svg';
+import './App.css';
+import React from "react"
+import Navbar from "./components/Navbar"
+import Hero from "./components/Hero"
+import Card  from './components/Card';
+//import pic from "./images/kedie.png"
+import data from "./data";
+
+
+export default function App() {
+  //
+
+  const cards = data.map(item =>{
+    return (
+      <Card 
+        key = {item.id}
+        {...item}
+      />
+    )
+  })
+  return (
+    <div>
+      <Navbar />
+      <Hero />
+      <section className="cards-list">
+        {cards}
+      </section>
+      
+    </div>
+  )
+}
+
+
---
+++ airbnb/src/components/Navbar.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+ export default function Navbar(){
+    return(
+        <nav>
+            <img src="../images/airbnb_img.png" className="nav--logo"/>
+        </nav>
+    )
+ }
